8005a2cdc4524ba8475547ceeeadeec0e5fc1132
Heiko Martini <dev.heikomartini@gmail.com>
2022-07-10 13:40:41 +0200

    Removes unused service

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,12 +1,10 @@
-import {Controller, Get, Res} from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
 import { AppService } from './app.service';
-import {ConfigService} from "@nestjs/config";
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private configService: ConfigService,
   ) {}
 
   @Get()
